05ea5d9800b7c5f8e5d763b57646130659b1edc1
cxz <2405819294@qq.com>
2022-05-26 15:46:06 +0800

    deploy

---
+++ precache-manifest.30795d6367c793a06a125584f0b150a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a119b8f3213bdb4c40f",
+    "url": "/MoMo-website/css/app.056124a4.css"
+  },
+  {
+    "revision": "33d6e750e254304ff9ef39da7596f842",
+    "url": "/MoMo-website/index.html"
+  },
+  {
+    "revision": "3a119b8f3213bdb4c40f",
+    "url": "/MoMo-website/js/app.30b0f298.js"
+  },
+  {
+    "revision": "76ae286ca783cbe21e9e",
+    "url": "/MoMo-website/js/chunk-vendors.5d99f9ca.js"
+  },
+  {
+    "revision": "56b15fe7ab344e8329250193ca9ffb6a",
+    "url": "/MoMo-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MoMo-website/robots.txt"
+  }
+]);
